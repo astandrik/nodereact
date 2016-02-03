7871541c3becbcebac63b94960cdff1c0f8d6d4d
astandrik <astandrik@gmail.com>
2016-02-03 15:17:27 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 var express = require("express"),
     app     = express();
-    
+
     config = require('config')(app),
     router = require('router')(app);
 
 
 
 
-app.listen(11960);
+app.listen(process.env.PORT || 8080);
 console.log('http://127.0.0.1:11960');
