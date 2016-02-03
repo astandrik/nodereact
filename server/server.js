e21527affd1d6de2799b742af580ba132ea3d64c
astandrik <astandrik@gmail.com>
2016-02-03 15:35:00 +0300

    Configured for heroku

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 var express = require("express"),
     app     = express();
-    
+
     config = require('config')(app),
     router = require('router')(app);
 
 
 
-
-app.listen(11960);
-console.log('http://127.0.0.1:11960');
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log('http://127.0.0.1:' + port);
