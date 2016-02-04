4f385f1b22aa2ae6a0f94f1ab934ce18fd79c51b
Anton <astandrik@gmail.com>
2016-02-05 01:48:44 +0300

    tryharding sockets

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 var express = require("express"),
-    app     = express();
+    app     = express(),
+    io = require('socket.io')
 
-    config = require('config')(app),
-    router = require('router')(app);
+var emitter = {}
 
 
+
+var config = require('config')(app, emitter),
+    router = require('router')(app, emitter);
 
 var port = process.env.PORT || 8080;
-app.listen(port);
+var server = app.listen(port);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+io(server).sockets.on('connection', function(socket) {
+  emitter.emit = function(message, data) {
+    socket.emit(message, data);
+  };
+});
 console.log('http://127.0.0.1:' + port);
