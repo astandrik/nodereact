16b44296afc292ac95d4aaf5ee264c994c62e8a8
Anton <astandrik@gmail.com>
2016-02-09 10:14:15 +0300

    deleted webgl-canvas

--- site/jsx/elements.jsx
+++ site/jsx/elements.jsx
@@ -4,14 +4,12 @@ module.exports = function(store) {
     var  Chat = require('./elements/Chat.jsx')(store),
          Textarea = require('./elements/Textarea.jsx')(store),
          Toolbar = require('./elements/Toolbar.jsx')(store),
-         UsersList = require('./elements/UsersList.jsx')(store),
-         Canvas = require('./elements/Canvas.jsx')(store);
+         UsersList = require('./elements/UsersList.jsx')(store);
 
   return {
     Chat,
     Textarea,
     Toolbar,
-    UsersList,
-    Canvas
+    UsersList
   };
 };
